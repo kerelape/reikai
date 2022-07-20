12668e9a2b616bf2289220aa0a4e25f3f29e20a4
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-20 07:38:50 +0500

    Create MemoryTests.kt

---
+++ src/test/kotlin/me/kerelape/reikai/io/MemoryTests.kt
@@ -0,0 +1,32 @@
+package me.kerelape.reikai.io
+
+import kotlinx.coroutines.runBlocking
+import me.kerelape.reikai.extentions.asEntity
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+import java.nio.ByteBuffer
+
+/**
+ * Test cases for [Heap].
+ *
+ * @since 0.0.0
+ */
+class MemoryTests {
+
+    /**
+     * Test that it puts data correctly.
+     */
+    @Test
+    fun `puts data`() = runBlocking {
+        val heap = Memory(
+            Heap(ByteBuffer.allocate(Long.SIZE_BYTES * 2)),
+            3.asEntity,
+            Long.SIZE_BYTES.asEntity
+        )
+        Assertions.assertEquals(
+            BigInteger(heap.put(128.asEntity).dataize()).toLong(),
+            BigInteger(heap.dataize()).toLong()
+        )
+    }
+}
