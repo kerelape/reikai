44a7abb6efdd9ebb57583e196b6400d4bb64cde5
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-22 08:31:17 +0500

    Update True.kt

--- src/main/kotlin/me/kerelape/reikai/logic/True.kt
+++ src/main/kotlin/me/kerelape/reikai/logic/True.kt
@@ -1,18 +1,11 @@
 package me.kerelape.reikai.logic
 
 import me.kerelape.reikai.core.Entity
+import me.kerelape.reikai.core.EntityWrap
 
 /**
  * Truth object that always dataizes as 1.
  *
  * @since 0.0.0
  */
-object True : Entity {
-
-    /**
-     * @return [ByteArray] that contains a single byte, which equals to 1.
-     */
-    override suspend fun dataize(): ByteArray {
-        return byteArrayOf(1)
-    }
-}
+object True : EntityWrap(Entity { byteArrayOf(1) })
