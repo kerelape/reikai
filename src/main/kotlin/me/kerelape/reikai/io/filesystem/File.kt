ad1bce354103b6b75bbef0e6da4b155ccb12b05c
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-22 12:07:43 +0500

    Update File.kt

--- src/main/kotlin/me/kerelape/reikai/io/filesystem/File.kt
+++ src/main/kotlin/me/kerelape/reikai/io/filesystem/File.kt
@@ -6,7 +6,6 @@ import me.kerelape.reikai.core.Entity
 import me.kerelape.reikai.io.RandomAccessChannel
 import me.kerelape.reikai.io.RandomAccessSource
 import me.kerelape.reikai.logic.False
-import java.net.URI
 import java.nio.channels.AsynchronousFileChannel
 import java.nio.file.OpenOption
 import java.nio.file.Path
@@ -26,7 +25,7 @@ class File(private val path: Entity, private vararg val options: OpenOption) : R
     }
 
     override suspend fun open(): RandomAccessChannel {
-        val path = Path.of(URI(String(this.path.dataize())))
+        val path = Path.of(String(this.path.dataize()))
         val channel = withContext(Dispatchers.IO) {
             AsynchronousFileChannel.open(path, *options)
         }
