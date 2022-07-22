be5290c770fa023267eb09f11a79ba720a187962
Max Smirnov <masynchin@gmail.com>
2022-07-22 04:38:01 +0300

    Add tests for Text

---
+++ src/test/kotlin/me/kerelape/reikai/text/TextTest.kt
@@ -0,0 +1,26 @@
+package me.kerelape.reikai.text
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Test cases for [Text].
+ *
+ * @since 0.0.0
+ */
+class TextTest {
+
+    /**
+     * Test that [String] can wrap a [Text].
+     */
+    @Test
+    fun `wraps by string`() = runBlocking {
+        Assertions.assertEquals(
+            "Hello, мир!",
+            String(
+                Text("Hello, мир!").dataize()
+            )
+        )
+    }
+}
