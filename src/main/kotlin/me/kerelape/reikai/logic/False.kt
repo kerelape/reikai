f64319afcae5e4b9230cecc1ccb66236c9c32971
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-22 08:31:19 +0500

    Update False.kt

--- src/main/kotlin/me/kerelape/reikai/logic/False.kt
+++ src/main/kotlin/me/kerelape/reikai/logic/False.kt
@@ -1,18 +1,11 @@
 package me.kerelape.reikai.logic
 
 import me.kerelape.reikai.core.Entity
+import me.kerelape.reikai.core.EntityWrap
 
 /**
  * False object that always dataizes as 0.
  *
  * @since 0.0.0
  */
-object False : Entity {
-
-    /**
-     * @return [ByteArray] that contains a single byte, which equals to 0.
-     */
-    override suspend fun dataize(): ByteArray {
-        return byteArrayOf(0)
-    }
-}
+object False : EntityWrap(Entity { byteArrayOf(0) })
