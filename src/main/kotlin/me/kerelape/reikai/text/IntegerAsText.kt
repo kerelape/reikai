0adde0eae237120e149279fb601cefc4de282324
Max Smirnov <masynchin@gmail.com>
2022-07-23 04:24:53 +0300

    Remove dead code

--- src/main/kotlin/me/kerelape/reikai/text/IntegerAsText.kt
+++ src/main/kotlin/me/kerelape/reikai/text/IntegerAsText.kt
@@ -4,11 +4,4 @@ import me.kerelape.reikai.core.Entity
 import me.kerelape.reikai.core.EntityWrap
 import java.math.BigInteger
 
-//class IntegerAsText(private val t: Text): EntityWrap(t) {
-//
-//    constructor(n: BigInteger): this(Text(n.toString()))
-//
-//    constructor(source: Entity):
-//}
-
 class IntegerAsText(source: Entity) : EntityWrap(Entity { Text(BigInteger(source.dataize()).toString()).dataize() })
