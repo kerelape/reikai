d1bbb13f9284f0806c9023138917fc9b9d7254b2
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-24 14:42:51 +0500

    Create EntityExtensionsTest.kt

---
+++ src/test/kotlin/me/kerelape/reikai/extentions/EntityExtensionsTest.kt
@@ -0,0 +1,29 @@
+package me.kerelape.reikai.extentions
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ * Test cases for Entity extensions.
+ *
+ * @since 0.0.0
+ */
+internal class EntityExtensionsTest {
+
+    /**
+     * Test that [toBoolean] converts to true correctly.
+     */
+    @Test
+    fun `convert to true correctly`() = runBlocking {
+        Assertions.assertTrue(4321.asEntity.toBoolean())
+    }
+
+    /**
+     * Test that [toBoolean] converts to false correctly.
+     */
+    @Test
+    fun `convert to false correctly`() = runBlocking {
+        Assertions.assertFalse(0.asEntity.toBoolean())
+    }
+}
