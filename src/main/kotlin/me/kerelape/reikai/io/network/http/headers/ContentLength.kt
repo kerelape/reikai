cc4180738abf2bbedc4808f73181e0db938cfcdf
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-24 09:29:50 +0500

    Update ContentLength.kt

--- src/main/kotlin/me/kerelape/reikai/io/network/http/headers/ContentLength.kt
+++ src/main/kotlin/me/kerelape/reikai/io/network/http/headers/ContentLength.kt
@@ -5,6 +5,11 @@ import me.kerelape.reikai.core.EntityWrap
 import me.kerelape.reikai.text.IntegerAsText
 import me.kerelape.reikai.text.Text
 
+/**
+ * Content-Length.
+ *
+ * @since 0.0.0
+ */
 class ContentLength(length: Entity) : EntityWrap(
     HttpHeader(Text("Content-Length"), IntegerAsText(length))
 )
