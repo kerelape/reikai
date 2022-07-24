b638076217bdd72e3b4e5b4f3ba995458cafa9d3
Max Smirnov <masynchin@gmail.com>
2022-07-25 00:53:06 +0300

    Test for false against bytearray of zeros

--- src/test/kotlin/io/github/kerelape/reikai/extentions/EntityExtensionsTest.kt
+++ src/test/kotlin/io/github/kerelape/reikai/extentions/EntityExtensionsTest.kt
@@ -1,5 +1,6 @@
 package io.github.kerelape.reikai.extentions
 
+import io.github.kerelape.reikai.core.Data
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -20,10 +21,18 @@ internal class EntityExtensionsTest {
     }
 
     /**
-     * Test that [toBoolean] converts to false correctly.
+     * Test that [toBoolean] converts zero to false.
      */
     @Test
-    fun `convert to false correctly`() = runBlocking {
+    fun `convert zero to false`() = runBlocking {
         Assertions.assertFalse(0.asEntity.toBoolean())
+    }
+
+    /**
+     * Test that [toBoolean] converts bytearray of zeros to false.
+     */
+    @Test
+    fun `convert bytearray of zeros to false`() = runBlocking {
+        Assertions.assertFalse(Data(byteArrayOf(0, 0, 0)).toBoolean())
     }
 }
