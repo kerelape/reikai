2b960f2a8515488426bcf29e8ddd5978424ecf57
Max Smirnov <masynchin@gmail.com>
2022-07-26 23:24:30 +0300

    Add And

---
+++ src/test/kotlin/io/github/kerelape/reikai/binary/AndTest.kt
@@ -0,0 +1,29 @@
+package io.github.kerelape.reikai.binary
+
+import io.github.kerelape.reikai.extentions.asEntity
+import io.github.kerelape.reikai.logic.Xor
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+/**
+ * Test cases for [And].
+ *
+ * @since 0.0.0
+ */
+class AndTest {
+
+    /**
+     * Test that it works correctly.
+     */
+    @Test
+    fun works() = runBlocking {
+        Assertions.assertEquals(
+            BigInteger(
+                Xor(0b11011.asEntity, 0b10011.asEntity, 0b11001.asEntity).dataize()
+            ).toInt(),
+            0b10001
+        )
+    }
+}
