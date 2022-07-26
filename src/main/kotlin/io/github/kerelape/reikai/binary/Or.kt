d3375e5c7f47dbad7b4d1ecf8b940559622b88e9
Max Smirnov <masynchin@gmail.com>
2022-07-26 23:27:43 +0300

    Add Or

---
+++ src/main/kotlin/io/github/kerelape/reikai/binary/Or.kt
@@ -0,0 +1,24 @@
+package io.github.kerelape.reikai.binary
+
+import io.github.kerelape.reikai.core.Entity
+import java.math.BigInteger
+
+/**
+ * Or.
+ *
+ * @since 0.0.1
+ */
+class Or(private vararg val elements: Entity) : Entity {
+
+    override suspend fun dataize(): ByteArray {
+        if (this.elements.size < 2) {
+            throw IllegalStateException("elements must contain at least 2 entities")
+        }
+        var previous = this.elements[0].dataize()
+        for (index in 1 until this.elements.size) {
+            val next = BigInteger(this.elements[index].dataize())
+            previous = BigInteger(previous).or(next).toByteArray()
+        }
+        return previous
+    }
+}
---
+++ src/test/kotlin/io/github/kerelape/reikai/binary/OrTest.kt
@@ -0,0 +1,29 @@
+package io.github.kerelape.reikai.binary
+
+import io.github.kerelape.reikai.extentions.asEntity
+import io.github.kerelape.reikai.logic.Xor
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+/**
+ * Test cases for [Or].
+ *
+ * @since 0.0.0
+ */
+class OrTest {
+
+    /**
+     * Test that it works correctly.
+     */
+    @Test
+    fun works() = runBlocking {
+        Assertions.assertEquals(
+            BigInteger(
+                Xor(0b1000.asEntity, 0b0010.asEntity, 0b0001.asEntity).dataize()
+            ).toInt(),
+            0b1011
+        )
+    }
+}
