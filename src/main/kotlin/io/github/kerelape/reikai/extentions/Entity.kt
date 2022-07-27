b8ca9f34749f35d65e9833888e05f91394df0292
kerelape <92184297+kerelape@users.noreply.github.com>
2022-07-27 06:24:07 +0500

    Update Entity.kt

--- src/main/kotlin/io/github/kerelape/reikai/extentions/Entity.kt
+++ src/main/kotlin/io/github/kerelape/reikai/extentions/Entity.kt
@@ -1,6 +1,7 @@
 package io.github.kerelape.reikai.extentions
 
 import io.github.kerelape.reikai.core.Entity
+import io.github.kerelape.reikai.logic.False
 import java.math.BigInteger
 
 /**
@@ -9,5 +10,9 @@ import java.math.BigInteger
  * @since 0.0.0
  */
 suspend fun Entity.toBoolean(): Boolean {
-    return BigInteger(this.dataize()) != BigInteger.ZERO
+    return try {
+        BigInteger(this.dataize()) != BigInteger.ZERO
+    } catch (exception: NumberFormatException) {
+        False
+    }
 }
