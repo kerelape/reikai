8c0e483951f8280704984ec33e835b83788133af
Max Smirnov <masynchin@gmail.com>
2022-07-28 20:07:14 +0300

    Fix Identity

--- src/main/kotlin/io/github/kerelape/reikai/logic/Identity.kt
+++ src/main/kotlin/io/github/kerelape/reikai/logic/Identity.kt
@@ -23,10 +23,8 @@
  */
 package io.github.kerelape.reikai.logic
 
-import io.github.kerelape.reikai.binary.Xor
 import io.github.kerelape.reikai.core.Entity
 import io.github.kerelape.reikai.core.EntityWrap
-import java.math.BigInteger
 
 /**
  * Equality of entities.
@@ -35,6 +33,6 @@ import java.math.BigInteger
  */
 class Identity(first: Entity, second: Entity) : EntityWrap(
     Entity {
-        (if (BigInteger(Xor(first, second).dataize()) == BigInteger.ZERO) True else False).dataize()
+        first.dataize().contentEquals(second.dataize()).asEntity.dataize()
     }
 )
