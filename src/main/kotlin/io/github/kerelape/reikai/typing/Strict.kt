92b803be4bc7848fb7a866e94afac1177b39fe49
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-28 18:15:48 +0500

    Update Strict.kt

--- src/main/kotlin/io/github/kerelape/reikai/typing/Strict.kt
+++ src/main/kotlin/io/github/kerelape/reikai/typing/Strict.kt
@@ -3,6 +3,7 @@ package io.github.kerelape.reikai.typing
 /**
  * Describes type of data.
  *
+ * @todo #71 Create a ktlint check. This check will throw a warning if types differ.
  * @since 0.1.0
  */
 @Target(
