7c935da5a064b65b7003a06b3baf3c028ff78185
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-28 12:33:06 +0500

    Refactor Equality
    So now it only takes two arguments

--- src/main/kotlin/io/github/kerelape/reikai/logic/Equality.kt
+++ src/main/kotlin/io/github/kerelape/reikai/logic/Equality.kt
@@ -10,6 +10,6 @@ import java.math.BigInteger
  *
  * @since 0.0.0
  */
-class Equality(vararg entities: Entity) : EntityWrap(Entity {
-    (if (BigInteger(Xor(*entities).dataize()) == BigInteger.ZERO) True else False).dataize()
+class Equality(first: Entity, second: Entity) : EntityWrap(Entity {
+    (if (BigInteger(Xor(first, second).dataize()) == BigInteger.ZERO) True else False).dataize()
 })
