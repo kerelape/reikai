02b646044e6a3876990ecb5941cff35415eb7b4f
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-28 18:10:33 +0500

    Create Type.kt

---
+++ src/main/kotlin/io/github/kerelape/reikai/typing/Type.kt
@@ -0,0 +1,30 @@
+package io.github.kerelape.reikai.typing
+
+/**
+ * Type of data.
+ *
+ * @since 0.1.0
+ */
+enum class Type {
+
+    /**
+     * Strict number.
+     *
+     * @since 0.1.0
+     */
+    INTEGER,
+
+    /**
+     * Logical value.
+     *
+     * @since 0.1.0
+     */
+    BOOLEAN,
+
+    /**
+     * Text.
+     *
+     * @since 0.1.0
+     */
+    TEXT,
+}
