94e96ec1506d0b9c1fa1f89ddedd4584b144370c
Max Smirnov <masynchin@gmail.com>
2022-07-30 05:16:15 +0300

    Add doc for Every

--- core/src/main/kotlin/io/github/kerelape/reikai/logic/Every.kt
+++ core/src/main/kotlin/io/github/kerelape/reikai/logic/Every.kt
@@ -3,4 +3,9 @@ package io.github.kerelape.reikai.logic
 import io.github.kerelape.reikai.core.Entity
 import io.github.kerelape.reikai.core.EntityWrap
 
+/**
+ * Every. [True] if all elements is [True].
+ *
+ * @since 0.1.0
+ */
 class Every(vararg entities: Entity) : EntityWrap(Entity { entities.all { it.toBoolean() }.asEntity.dataize() })
