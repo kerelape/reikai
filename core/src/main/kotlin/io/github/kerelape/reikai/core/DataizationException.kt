dc09c95e9a728ab8791e346ce60d632de472f9b1
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-08-02 22:40:08 +0500

    Create DataizationException.kt

---
+++ core/src/main/kotlin/io/github/kerelape/reikai/core/DataizationException.kt
@@ -0,0 +1,8 @@
+package io.github.kerelape.reikai.core
+
+/**
+ * Dataization exception. Thrown when an entity could not be dataized for a reason.
+ *
+ * @since 0.2.0
+ */
+class DataizationException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause)
