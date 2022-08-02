963ca0c201b490d7d72c3d5e5b7f95c0b55b00bd
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-08-02 23:23:29 +0500

    Create ErrorTest.kt

---
+++ core/src/test/kotlin/io/github/kerelape/reikai/core/ErrorTest.kt
@@ -0,0 +1,35 @@
+package io.github.kerelape.reikai.core
+
+import io.github.kerelape.reikai.text.Text
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ * Test cases for [Error].
+ *
+ * @since 0.2.0
+ */
+internal class ErrorTest {
+
+    @Test
+    fun `throws DataizationException when dataizes`() {
+        Assertions.assertThrows(DataizationException::class.java) {
+            runBlocking {
+                Error().dataize()
+            }
+        }
+    }
+
+    @Test
+    fun `thrown exceptions contains correct message`() = runBlocking {
+        try {
+            Error(Text("abc")).dataize()
+        } catch (exception: DataizationException) {
+            Assertions.assertEquals(
+                "abc",
+                exception.message
+            )
+        }
+    }
+}
