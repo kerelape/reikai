f47ef46768c915f97fdad534769e4d084b82fb8d
kerelape <kerelape@gmail.com>
2022-12-10 13:30:56 +0500

    Add doc

--- core/src/main/kotlin/io/github/kerelape/reikai/guards/StructGuard.kt
+++ core/src/main/kotlin/io/github/kerelape/reikai/guards/StructGuard.kt
@@ -4,6 +4,11 @@ import io.github.kerelape.reikai.core.Entity
 import io.github.kerelape.reikai.core.EntityWrap
 import io.github.kerelape.reikai.logic.Fork
 
+/**
+* StructGuard. Enchures that [origin] is correct length.
+*
+* @since 0.2.0
+ */
 class StructGuard(
     origin: Entity,
     length: Entity,
